3db8a351191f5f7bae3a447a41a8aa8aa04c1877
Apple <appleteng97@gmail.com>
2022-07-08 16:27:51 +0800

    refactor: add lodash to group data by list

--- controller/cardController.js
+++ controller/cardController.js
@@ -1,4 +1,5 @@
 //Declaration
+const _ = require('lodash');
 const axios = require('axios')
 const url = 'https://api.trello.com/1/boards/609f488638122f7a82bf31b4?key=e4ca0224f7ed7ba9dceac38b122ef10e&token=22dc795b770cc5fe1a66ee042ebd69caec1a8276917963e6ececd5ac20263ccd&fields=all&actions=all&action_fields=all&actions_limit=1000&cards=all&card_fields=all&card_attachments=true&labels=all&lists=all&list_fields=all&members=all&member_fields=all&checklists=all&checklist_fields=all&organization=false'
 
@@ -74,16 +75,9 @@ function generateReport(request,response){
         )
     }
 
-    const groupBy = (arrCard, key) => {
-        // Return the end result
-        return arrCard.reduce((result, currentValue) => {
-          (result[currentValue[key]] = result[currentValue[key]] || []).push(currentValue);
-          return result;
-        }, {});
-    };
-      
-    const sortedCard = groupBy(updatedCard, 'updatedListName');
+    var sortedCard = _.groupBy(updatedCard,'updatedListName');
     response.json(sortedCard);
+
   };
 
   axios
