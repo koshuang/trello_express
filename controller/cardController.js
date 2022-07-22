77a8dd7eec06192832579b61cf0d0e8efdfa876d
Apple <appleteng97@gmail.com>
2022-07-22 19:05:52 +0800

    amend to group by each labels of each card

--- controller/cardController.js
+++ controller/cardController.js
@@ -91,30 +91,63 @@ function generateReport(request,response){
     }
 
     //Group by label
+    let arrLabel = []
     for (let cardStatus in sortedCard){
         for (let cardMonth in sortedCard[cardStatus])
         {
-            sortedCard[cardStatus][cardMonth] = _.groupBy(sortedCard[cardStatus][cardMonth], data=>{
-                labelList = data.labels
-                if(labelList.length > 0){
-                    for (var b=0 ; b<labelList.length; b++)
-                    {
-                        //console.log(labelList.length + " and this is " + b)
-                        //console.log(labelStatus[b].name)
-                        return labelList[b].name
-                    }
-                }
-                else{
-                    return "No Label"
+            //put label list into array
+            var result = sortedCard[cardStatus][cardMonth] //store array of obj created on certain month to variable
+            _.forEach(result, eachCard =>{ //each card in the result array under certain month
+                if (eachCard.labels.length > 0){
+                    _.forEach(eachCard.labels, labelList=>{ //for each item in the labels array of certain card
+                        if(!_.includes(arrLabel, labelList.name)){
+                            arrLabel.push(labelList.name)
+                        }
+                    })
                 }
             })
 
+            //add list key to sorted array + add value
+            var arrCard = sortedCard[cardStatus][cardMonth] //array of cards under certain month
+            var objCardByLabel = {}
+            arrCard.forEach(eachCard =>{ //run through each card
+                var labelList = eachCard.labels
+                if(labelList.length > 0){ //if got label
+                    for (var b=0 ; b<labelList.length; b++){ //each label of current card
+                        if (arrLabel.includes(labelList[b].name)){
+                            var index = arrLabel.indexOf(labelList[b].name)
+                            if (arrLabel[index] in objCardByLabel){
+                                var arr = objCardByLabel[arrLabel[index]]
+                                arr.push(eachCard.name)
+                                objCardByLabel[arrLabel[index]] = arr
+                            }
+                            else{
+                                objCardByLabel[arrLabel[index]] = [eachCard.name]
+                            }
+                        }
+                    }
+                }
+                else{
+                    if ("No Label" in objCardByLabel){
+                        var arr = objCardByLabel["No Label"]
+                        arr.push(eachCard.name)
+                        objCardByLabel["No Label"] = arr
+                    }
+                    else{
+                        objCardByLabel["No Label"] = [eachCard.name]
+                    }
+                }
+            })
+
+            sortedCard[cardStatus][cardMonth] = objCardByLabel
+        
             //to display number of cards created
             for (let labelList in sortedCard[cardStatus][cardMonth]){
                 sortedCard[cardStatus][cardMonth][labelList] = _.size(sortedCard[cardStatus][cardMonth][labelList]);
             }
         }
     }
+
     response.json(sortedCard);
   };
 
@@ -124,4 +157,4 @@ function generateReport(request,response){
   .catch(catchError)
 }
 
-  module.exports = { generateReport }
+  module.exports = { generateReport}
